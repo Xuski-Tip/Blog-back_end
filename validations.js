7661deaddfb525793625ad493e71a45e5a2b6b76
Xuski-Tip <75438787+Xuski-Tip@users.noreply.github.com>
2022-07-29 18:11:08 +0500

    Add files via upload

---
+++ validations.js
@@ -0,0 +1,23 @@
+import {body} from 'express-validator'
+
+
+export const loginValidation = [
+    body('email').isEmail(),
+    body('password').isLength({min: 5}),
+   
+];
+export const registrValidation = [
+    body('email').isEmail(),
+    body('password').isLength({min: 5}),
+    body('fullName').isLength({min: 3}),
+    body('avatarUrl').optional().isURL(),
+];
+export const postCreateValidation = [
+    body('title', 'Введите заголовок статьи').isLength({min:3}).isString(),
+    body('text', 'Введите текст статьи').isLength({min: 10}).isString(),
+    body('tags', 'Неверный формат тэгов(укажите массив)').optional().isString(),
+    body('imageUrl', 'Неверная ссылка на изображение' ).optional().isString(),
+];
+
+
+
